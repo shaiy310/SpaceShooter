1040a072d6c6d85ca8416951113857c122197152
shai yaakovi <shaiy310@gmail.com>
2024-04-11 15:49:24 +0300

    Add weapon WIP

--- Assets/Scripts/PlayerRayCast.cs
+++ Assets/Scripts/PlayerRayCast.cs
@@ -10,24 +10,38 @@ public class PlayerRayCast : MonoBehaviour
     int maxDistance;
     [SerializeField] GameObject cameraPos;
     [SerializeField] LayerMask rayCastHitable;
+    Dictionary<State, Vector3> shootingPositions;
+
+    // Weapons
+    [SerializeField] WeaponBase weapon;
 
     // Start is called before the first frame update
     void Start()
     {
         maxDistance = 500;
-        Cursor.visible = false;
+        shootingPositions = new Dictionary<State, Vector3>() {
+            { State.Standing, new Vector3(.25f, 1.6f, 1.5f) }
+        };
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log("TODO: get state from player movement script");
+        State state = State.Standing;
+
         if (Input.GetMouseButton(0))
         {
             origin = cameraPos.GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
-            if (Physics.Raycast(origin, cameraPos.transform.forward, out hit, maxDistance, rayCastHitable))
-            {
-                hit.transform.GetComponent<MeshRenderer>().material.color = Color.red;
-            }
+            Instantiate(weapon.Ammo.Bullet,
+                transform.position + shootingPositions[state],
+                Quaternion.AngleAxis(90, Vector3.right)
+            );
+
+            //if (Physics.Raycast(origin, cameraPos.transform.forward, out hit, maxDistance, rayCastHitable)) {
+            //    hit.transform.GetComponent<MeshRenderer>().material.color = Color.red;
+
+            //}
         }
     }
 }
