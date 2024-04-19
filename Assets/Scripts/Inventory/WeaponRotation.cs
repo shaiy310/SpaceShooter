484ff5448701b907b76c0ef6c53ac82aa39ae748
YakirKors <yakirkors291@gmail.com>
2024-04-19 18:59:02 +0300

    Weapon Rotation in inventory

--- Assets/Scripts/Inventory/WeaponRotation.cs
+++ Assets/Scripts/Inventory/WeaponRotation.cs
@@ -6,40 +6,31 @@ public class WeaponRotation : MonoBehaviour
 {
     // Variables
     public float rotationSpeed;
-    private bool isDragging;
-    private Vector3 lastMousePosition;
+    [SerializeField] Camera cam;
 
     // Start is called before the first frame update
     void Start()
     {
-        rotationSpeed = 1f;
+        rotationSpeed = 10f;
+    }
+
+    void OnMouseDrag()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            float rotateX = Input.GetAxis("Mouse X") * rotationSpeed;
+            float rotateY = Input.GetAxis("Mouse Y") * rotationSpeed;
+
+            Vector3 right = Vector3.Cross(cam.transform.up, transform.position - cam.transform.position);
+            Vector3 up = Vector3.Cross(transform.position - cam.transform.position, right);
+
+            transform.rotation = Quaternion.AngleAxis(-rotateX, up) * transform.rotation;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        SpinWeapon();
-    }
-
-    void SpinWeapon()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            isDragging = true;
-            lastMousePosition = Input.mousePosition;
-        }
-        else if (Input.GetMouseButtonUp(0))
-        {
-            isDragging = false;
-        }
-
-        if (isDragging)
-        {
-            Vector3 currentMousePosition = lastMousePosition;
-            Vector3 mouseDelta = currentMousePosition - lastMousePosition;
-
-            transform.Rotate(Vector3.up, -mouseDelta.x * rotationSpeed, Space.Self);
-            lastMousePosition = currentMousePosition;
-        }
+        OnMouseDrag();
     }
 }
