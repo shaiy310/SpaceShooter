97bb5a025881f27510ca9be5139af5a90dd183db
YakirKors <yakirkors291@gmail.com>
2024-05-09 20:15:59 +0300

    f

--- Assets/Scripts/Weapons/WeaponStats.cs
+++ Assets/Scripts/Weapons/WeaponStats.cs
@@ -7,14 +7,6 @@ using UnityEngine;
 public class WeaponStats
 {
     [SerializeField] float damage;
-    [SerializeField] float clipSize;
-    [SerializeField] float accuracy;
-    [SerializeField] float range;
-    [SerializeField] bool isAuto;
 
     public float Damage { get { return damage; } set {  damage = value; } }
-    public float ClipSize { get { return clipSize; } set { clipSize = value; } }
-    public float Accuracy { get { return accuracy; } set { accuracy = value; } }
-    public float Range { get { return range; } set { range = value; } }
-    public bool IsAuto => isAuto;
 }
