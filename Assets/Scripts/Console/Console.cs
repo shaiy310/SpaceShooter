b8ff750c22dcf4c2bae027eb49d395e0a88a404f
shaiy310 <shaiy310@gmail.com>
2024-05-27 18:22:01 +0300

    update console handling

--- Assets/Scripts/Console/Console.cs
+++ Assets/Scripts/Console/Console.cs
@@ -6,31 +6,15 @@ using UnityEngine.Events;
 
 public class Console : MonoBehaviour, IInteractable
 {
-    [SerializeField] ScreenMessage screen;
     [SerializeField] UnityEvent action;
-
-    //[SerializeField] 
-
 
     public void Interact()
     {
         action?.Invoke();
     }
 
-    public void IncreaseShield()
-    {
-        screen.PostMessage("shield up");
-    }
-
     public void UpdateAmmo(PlayerRayCast shooter)
     {
-        screen.PostMessage("new ammo");
         shooter.SwitchWeapon();
-    }
-
-    public void UnlockDoor(RegularDoorEngine door)
-    {
-        screen.PostMessage("Engineering door unlocked");
-        door.enabled = true;
     }
 }
