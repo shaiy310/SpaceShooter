04c1701cf8df9c98957b6cf804aa66d93f9dd6e3
shaiy310 <shaiy310@gmail.com>
2024-05-28 11:32:05 +0300

    Add oxygen console storyline (#11)
    * Add Oxygen Machine to storyline
    
    * update console handling
    
    * fix health restoration screen

--- Assets/Scripts/Console/Console.cs
+++ Assets/Scripts/Console/Console.cs
@@ -6,28 +6,10 @@ using UnityEngine.Events;
 
 public class Console : MonoBehaviour, IInteractable
 {
-    [SerializeField] ScreenMessage screen;
     [SerializeField] UnityEvent action;
 
     public void Interact()
     {
         action?.Invoke();
-    }
-
-    public void IncreaseShield()
-    {
-        screen.PostMessage("shield up");
-    }
-
-    public void UpdateAmmo(WeaponBase weaponBase)
-    {
-        screen.PostMessage("new ammo");
-        PlayerRayCast.instance.SwitchWeapon(weaponBase);
-    }
-
-    public void UnlockDoor(RegularDoorEngine door)
-    {
-        screen.PostMessage("Engineering door unlocked");
-        door.enabled = true;
     }
 }
