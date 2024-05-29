1cdcc2e860d17ac500702cfec3874b6352844644
shaiy310 <shaiy310@gmail.com>
2024-05-29 17:50:43 +0300

    Added patrol capabilities to enemy

--- Assets/Scripts/RobotSphereEngine.cs
+++ Assets/Scripts/RobotSphereEngine.cs
@@ -4,23 +4,45 @@ using UnityEngine;
 
 public class RobotSphereEngine : MonoBehaviour
 {
+    [SerializeField] List<Vector3> Routes;
+
     Animator animator;
+    float speed;
+
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
-        StartCoroutine(WalkAnimation());
+        speed = 2f;
+
+        if (Routes.Count > 0) {
+            StartCoroutine(Patrol());
+        }
     }
 
-    IEnumerator WalkAnimation()
+    IEnumerator Patrol()
     {
         while (true) {
-            animator.SetBool("Walk_Anim", true);
-            yield return new WaitForSeconds(5);
+            foreach (var route in Routes) {
+                animator.SetBool("Walk_Anim", true);
+                yield return Walk(route);
 
-            animator.SetBool("Walk_Anim", false);
-            yield return new WaitForSeconds(2);
+                animator.SetBool("Walk_Anim", false);
+                yield return new WaitForSeconds(2);
+            }
 
+        }
+    }
+
+    IEnumerator Walk(Vector3 delta)
+    {
+        Vector3 target = transform.position + delta;
+        Vector3 speedVector = speed * delta.normalized;
+
+        transform.LookAt(target);
+        while (Vector3.Distance(transform.position, target) > 0.25f) {
+            transform.position += Time.deltaTime * speedVector;
+            yield return null;
         }
     }
 }
