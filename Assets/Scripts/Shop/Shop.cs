9263ce92baea6e00f394d047572a22a3b020aa96
YakirKors <166528050+YakirKors@users.noreply.github.com>
2024-05-30 20:54:46 +0300

    Inventory, Shop, Coins (#12)
    Added inventory
    Added shop
    Added coins (wip)
    the last choice in inventory is the one you get in to the game with
    
    Co-authored-by: shaiy310 <shaiy310@gmail.com>

---
+++ Assets/Scripts/Shop/Shop.cs
@@ -0,0 +1,141 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    //Variables
+    [SerializeField] Material[] materials;
+    [SerializeField] Renderer weaponPreview;
+    [SerializeField] Renderer[] bodyPreview;
+    [SerializeField] Renderer materialWeaponPreview;
+    [SerializeField] Renderer materialBodyPreview;
+
+    [SerializeField] TextMeshProUGUI coin;
+    [SerializeField] TextMeshProUGUI coinToBuyWeapon;
+    [SerializeField] TextMeshProUGUI coinToBuyBody;
+
+    [SerializeField] GameObject mainMenu;
+    [SerializeField] GameObject shop;
+
+    [SerializeField] Inventory newMaterials;
+
+    private int WeaponIndex;
+    private int bodyIndex;
+
+    private void Start()
+    {
+        WeaponIndex = 0;
+        bodyIndex = 0;
+        
+        StartCoroutine(ShowCoins());
+
+        weaponPreview.material = materials[WeaponIndex];
+        materialWeaponPreview.material = materials[WeaponIndex];
+
+        materialBodyPreview.material = materials[bodyIndex];
+
+        foreach (var part in bodyPreview)
+        {
+            part.material = materials[bodyIndex];
+        }
+
+        newMaterials = FindObjectOfType<Inventory>();
+    }
+
+    IEnumerator ShowCoins()
+    {
+        yield return new WaitUntil(() => Coins.mainCoins > 0 && Coins.shopCoins > 0);
+
+        coin.text = Coins.mainCoins.ToString();
+        coinToBuyWeapon.text = Coins.shopCoins.ToString();
+        coinToBuyBody.text = Coins.shopCoins.ToString();
+    }
+
+    public void Back()
+    {
+        shop.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public int NextOption(int currentIndex)
+    {
+        currentIndex++;
+
+        if (currentIndex >= materials.Length)
+        {
+            currentIndex = 0;
+        }
+
+        return currentIndex;
+    }
+
+    public int PrevOption(int currentIndex)
+    {
+        currentIndex--;
+
+        if (currentIndex < 0)
+        {
+            currentIndex = materials.Length - 1;
+        }
+
+        return currentIndex;
+    }
+
+    public void UpdateWeaponMaterial(string direction)
+    {
+        if (direction == "Right")
+        {
+            WeaponIndex = NextOption(WeaponIndex);
+        }
+        else
+        {
+            WeaponIndex = PrevOption(WeaponIndex);
+        }
+
+        weaponPreview.material = materials[WeaponIndex];
+        materialWeaponPreview.material = materials[WeaponIndex];
+    }
+
+    public void UpdateBodyMaterial(string direction)
+    {
+        if (direction == "Right")
+        {
+            bodyIndex = NextOption(bodyIndex);
+        }
+        else
+        {
+            bodyIndex = PrevOption(bodyIndex);
+        }
+
+        materialBodyPreview.material = materials[bodyIndex];
+
+        foreach (var part in bodyPreview)
+        {
+            part.material = materials[bodyIndex];
+        }
+    }
+
+    public void BuyMaterial(string part)
+    {
+        Coins.mainCoins -= Coins.shopCoins;
+        coin.text = Coins.mainCoins.ToString();
+
+        AddMaterialToInventory(part);
+    }
+
+    public void AddMaterialToInventory(string part)
+    {
+        if (part == "Weapon")
+        {
+            newMaterials.WeaponMaterials.Add(materials[WeaponIndex]);
+        }
+        else
+        {
+            newMaterials.BodyMaterials.Add(materials[bodyIndex]);
+        }
+        
+    }
+}
+
